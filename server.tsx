96ce53c438e55ac0f2d2a39e107ba76b4190a5f3
Iago Sardiña <iago.sardina@landrasistemas.es>
2023-12-10 19:43:52 +0100

    allow to be in a subpath

--- server.tsx
+++ server.tsx
@@ -2,6 +2,7 @@ import { renderToReadableStream } from 'react-dom/server';
 import ServerPage from './server-page.tsx';
 
 const BASE_PATH = Bun.env.BASE_PATH;
+const RELATIVE_PATH = Bun.env.RELATIVE_PATH;
 const PORT = Bun.env.PORT || 3000;
 
 console.log(`Server started at port: ${PORT}`);
@@ -10,12 +11,13 @@ Bun.serve({
 
     async fetch(req) {
         const url = new URL(req.url);
+        const path = RELATIVE_PATH ? url?.pathname.replace(RELATIVE_PATH, '') : url?.pathname;
+        const file = Bun.file((BASE_PATH || '.') + decodeURI(path));
+
         console.log(
-            `${new Date().toISOString()} - ${req.method}:: ${url?.pathname} ${JSON.stringify(url.searchParams)}`
+            `${new Date().toISOString()} - ${req.method}:: ${path} ${JSON.stringify(url.searchParams)}`
         );
 
-        const file = Bun.file((BASE_PATH || '.') + decodeURI(url?.pathname));
-        
         //File is forced to be in the specified BASE_PATH or in the current working directory.
         if (await file.exists()) {
             return new Response(file);
