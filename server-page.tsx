657469b226914d4ee8ec70a64f112908110c6d33
Iago Sardiña <iago.sardina@landrasistemas.es>
2023-12-12 17:01:12 +0100

    Fix styles

--- server-page.tsx
+++ server-page.tsx
@@ -20,9 +20,16 @@ export default ({ type, rom, bios, core }: EmulatorProps) => {
     })}
     `;
 
+    const style = `
+        html, 
+        body {
+            height: 100%;
+        }
+    `
+
     return (
         <html>
-            <head style={{ height: '100%' }}>
+            <head >
                 <meta
                     name="viewport"
                     content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
@@ -30,8 +37,9 @@ export default ({ type, rom, bios, core }: EmulatorProps) => {
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
                 <script dangerouslySetInnerHTML={{ __html: script }} />
+                <style dangerouslySetInnerHTML={{ __html: style }} />
             </head>
-            <body style={{ margin: 0, background: 'black', color: 'white', height: '100%' }}>
+            <body style={{ margin: 0, background: 'black', color: 'white' }}>
                 <div id="root"></div>
                 <script type="module" src="./index.js"></script>
             </body>
